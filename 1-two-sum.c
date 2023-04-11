392aa7a615059a7ac8e8c2b49ec8961bb90dd651
bramtechs <bramtech@telenet.be>
2023-04-11 16:07:36 +0200

    Second solution 1: Add two numbers (use malloc)

--- 1-two-sum.c
+++ 1-two-sum.c
@@ -7,7 +7,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for (int i = 0; i < numsSize; i++) {
         for (int j = i+1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
-                int* result = calloc(2,sizeof(int));
+                int* result = malloc(2*sizeof(int));
                 result[0] = i;
                 result[1] = j;
                 *returnSize = 2;
