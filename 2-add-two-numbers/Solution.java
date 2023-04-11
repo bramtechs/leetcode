edeaea5d985edf8c5860efae13d67b5ac6391c6f
bramtechs <bramtech@telenet.be>
2023-04-11 21:58:17 +0200

    Progress on 2: add two numbers

--- 2-add-two-numbers/Solution.java
+++ 2-add-two-numbers/Solution.java
@@ -1,20 +1,21 @@
 public class Solution {
 
-    private int addNodes(ListNode n1, ListNode n2) {
-        int left = n1 == null ? 0:n1.val;
-        int right = n2 == null ? 0:n2.val;
-        return left + right;
-    }
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null)
+            return null;
+
         ListNode results = new ListNode();
         ListNode rn = results;
-        while(true) {
-            int sum = addNodes(l1,l2);
-            rn.val += sum % 10;
-            l1 = l1.next;
-            l2 = l2.next;
-            if (l1 == null || l2 == null) {
+        while (true) {
+            int left = l1 == null ? 0 : l1.val;
+            int right = l2 == null ? 0 : l2.val;
+            int sum = left + right;
+            rn.val += sum;
+            rn.val %= 10;
+
+            l1 = l1 == null ? null:l1.next;
+            l2 = l2 == null ? null:l2.next;
+            if (l1 == null && l2 == null) {
                 break;
             }
 
@@ -26,16 +27,17 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        ListNode results = new Solution().addTwoNumbers(l1,l2);
-        while(true){
-            System.out.print(Integer.toString(results.val) + " ");
-            if (results.next != null){
-                results = results.next;
-            } else {
-                break;
-            }
+        {
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            ListNode results = new Solution().addTwoNumbers(l1, l2);
+            System.out.println(results.toString());
+        }
+        {
+            ListNode l1 = ListNode.repeating(9, 7);
+            ListNode l2 = ListNode.repeating(9, 4);
+            ListNode results = new Solution().addTwoNumbers(l1, l2);
+            System.out.println(results.toString());
         }
     }
 }
