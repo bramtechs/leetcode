038f7d69dc3dc267ce9780347b6f8578c0a53a6d
bramtechs <bramtech@telenet.be>
2023-04-11 16:06:09 +0200

    Second solution 1: Add two numbers

--- 1-two-sum.c
+++ 1-two-sum.c
@@ -3,11 +3,9 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for (int i = 0; i < numsSize; i++) {
-        for (int j = 0; j < numsSize; j++) {
-            if (i == j) continue;
+        for (int j = i+1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
                 int* result = calloc(2,sizeof(int));
                 result[0] = i;
